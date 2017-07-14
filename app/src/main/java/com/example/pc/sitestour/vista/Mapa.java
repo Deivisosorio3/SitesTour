153f6d552e5a9b31544f9c9ed40ed37cf397ac60
deivisosorio3 <deiviseguiso@gmail.com>
2017-07-14 18:00:50 -0500

    sugar

--- app/src/main/java/com/example/pc/sitestour/vista/Mapa.java
+++ app/src/main/java/com/example/pc/sitestour/vista/Mapa.java
@@ -3,6 +3,7 @@ package com.example.pc.sitestour.vista;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.example.pc.sitestour.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
